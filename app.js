41af3001a675e44bcf297605ed74a2dcecb2961d
Sabir-kral <sabirmemmedli21152014@gmail.com>
2024-11-19 13:05:50 +0400

    ssdfdfga

---
+++ app.js
@@ -0,0 +1,51 @@
+let eded = prompt("1 ile 10 arsinda eded yaz")
+if(eded ==2) {
+    console.log("eded cut ededdir");
+} else{
+
+}
+if (eded ==1) {
+    console.log("eded tek ededdir");
+}else{
+
+}
+if (eded ==3) {
+    console.log("eded tek ededdir");
+}else{
+
+}
+if (eded ==5) {
+    console.log("eded tek ededdir");
+}else{
+
+}
+if (eded ==7) {
+    console.log("eded tek ededdir");
+}else{
+
+}
+if (eded ==9) {
+    console.log("eded tek ededdir");
+}else{
+
+}
+if(eded ==4) {
+    console.log("eded cut ededdir");
+} else{
+
+}
+if(eded ==6) {
+    console.log("eded cut ededdir");
+} else{
+
+}
+if(eded ==8) {
+    console.log("eded cut ededdir");
+} else{
+
+}
+if(eded ==10) {
+    console.log("eded cut ededdir");
+} else{
+
+}
